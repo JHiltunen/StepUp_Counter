2e8785f2707e7b95af79f6fb4fb8e1dce8563831
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-09-20 15:14:21 +0300

    Added ability to see your current step count. Number of steps will be saved to sharedPreferences

--- app/src/main/java/com/stepupcounter/stepupcounter/MainActivity.kt
+++ app/src/main/java/com/stepupcounter/stepupcounter/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.stepupcounter.stepupcounter
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    // request code for PHYSICAL_ACTIVITY
+    // the code can be any number
+    private val PHYSICAL_ACTIVITY = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_analytics, R.id.navigation_exercise))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Since API level 29 we are required to ask the user for ACTIVITY_RECOGNITION at runtime.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)!= PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            //ask for permission to use sensor if API level >= 29
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                requestPermissions(arrayOf(Manifest.permission.ACTIVITY_RECOGNITION), PHYSICAL_ACTIVITY)
+            }
+        } else {
+            // User has already given the permission
+            Toast.makeText(this, "Permission is already provided", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/stepupcounter/stepupcounter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/stepupcounter/stepupcounter/ui/home/HomeFragment.kt
@@ -1,23 +1,39 @@
 package com.stepupcounter.stepupcounter.ui.home
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.stepupcounter.stepupcounter.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SensorEventListener {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var sensorManager: SensorManager? = null
+    private lateinit var tv_stepsCount : TextView
+
+    // count number os steps as float value because progress bar animation needs float values
+    private var totalStepsSinceLastRebootOfDevice= 0f
+    private var previousTotalSteps = 0f
+    private var running = false
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
@@ -26,6 +42,123 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, {
             textView.text = it
         })
+
+        // get the SensorManager when creating the view
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        // find the TextView element (that shows value of steps) from root
+        tv_stepsCount = root.findViewById(R.id.tv_stepsTaken) as TextView
+
+        loadData()
+        resetSteps()
+
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        // get the Step Counter sensor from sensormanager
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            // step sensor not found on some (older) devices
+            // we need to use requireActivity() method to access context that's needed on Toast
+            Toast.makeText(this.requireActivity().applicationContext, "No sensor detected on this device", Toast.LENGTH_LONG).show()
+            Log.d("Steps", "Sensor not found")
+        } else {
+            // add listener for the sensor we use
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+            Log.d("Steps", "Added listener")
+        }
+    }
+
+    /**
+     * This function is called when sensor value changes/sensor detects movement.
+     * Calculating currentSteps by taking totalStepsSinceLastRebootOfDevice and subtracting previousTotalSteps from it.
+     * previousTotalSteps is retrieved from sharedPreferences when onCreateView function at the start is called.
+     * Calls circular progressbar setProgressAnimation to visualize step count.
+     */
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running) {
+            // event!! -> event not null
+            // value of sensor is at index 0
+            totalStepsSinceLastRebootOfDevice = event!!.values[0]
+
+            // previousTotalSteps is retrieved from sharedPreferences on loadData function when creating the view
+            // currentSteps = totalStepsSinceLastRebootOfDevice - previousTotalSteps
+            val currentSteps : Int = totalStepsSinceLastRebootOfDevice.toInt() - previousTotalSteps.toInt()
+            Log.d(
+                "Steps",
+                "totalStepsSinceLastRebootOfDevice: $totalStepsSinceLastRebootOfDevice : currentSteps: $currentSteps"
+            )
+            tv_stepsCount.text = currentSteps.toString()
+
+            Log.d("Steps", "currentSteps: $currentSteps")
+            // update progressbar animation
+            progress_circular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
+                Log.d("Steps", "setProgressWithAnimation")
+            }
+        } else {
+            Log.d("Steps", "Event null")
+        }
+    }
+
+    /**
+     * Not used on this project.
+     */
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    /**
+     * Fuction to reset steps to zero (0).
+     * Assigns onClickListener to tv_stepsCount TextView to show message, how to reset steps.
+     * Assign onLongClickListener to actually reset the value and then save the data to shared preferences by calling saveData() function.
+     */
+    private fun resetSteps() {
+        // when clicked the element that holds value of steps, show message how to reset the value
+        tv_stepsCount.setOnClickListener {
+            Toast.makeText(this.requireActivity().applicationContext, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_stepsCount.setOnLongClickListener {
+            previousTotalSteps = totalStepsSinceLastRebootOfDevice
+            tv_stepsCount.text = 0.toString()
+            // call saveData to save it to SharedPreferences
+            saveData()
+
+            true
+        }
+    }
+
+    /**
+     * Loads the number of steps saved to shared preferences with the key "key1"
+     * Name of sharedPrefrences is "myPrefs"
+     */
+    private fun loadData() {
+        // loads steps count from shared preferences
+        val sharedPrefrences : SharedPreferences = this.requireActivity().getSharedPreferences(
+            "myPrefs",
+            Context.MODE_PRIVATE
+        )
+        val savedNumber : Float = sharedPrefrences.getFloat("key1", 0f)
+        Log.d("Steps", "$savedNumber")
+        previousTotalSteps = savedNumber
+    }
+
+    /**
+     * Saves the number of steps in the shared preferences with the key "key1"
+     * Name of sharedPrefrences is "myPrefs"
+     */
+    private fun saveData() {
+        val sharedPrefrences : SharedPreferences = this.requireActivity().getSharedPreferences(
+            "myPrefs",
+            Context.MODE_PRIVATE
+        )
+        val editor : SharedPreferences.Editor = sharedPrefrences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+        Log.d("Steps", "saved data")
     }
 }
