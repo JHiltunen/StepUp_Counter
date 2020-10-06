c389fa49212b8d0d859e7ed0ea0e1a721d922ef2
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-10-06 23:08:25 +0300

    Removed unused import
    Signed-off-by: Joonas Hiltunen <joonas.s.jh@gmail.com>

--- app/src/main/java/com/jhiltunen/stepupcounter/ui/information/InformationFragment.kt
+++ app/src/main/java/com/jhiltunen/stepupcounter/ui/information/InformationFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.jhiltunen.stepupcounter.R
