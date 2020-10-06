f2e8497befbbdb02698e35423fbfd58e0bb51043
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-10-06 23:38:16 +0300

    Added Java Doc comments
    Signed-off-by: Joonas Hiltunen <joonas.s.jh@gmail.com>

--- app/src/main/java/com/jhiltunen/stepupcounter/data/models/Steps.kt
+++ app/src/main/java/com/jhiltunen/stepupcounter/data/models/Steps.kt
@@ -5,6 +5,14 @@ import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
+/**
+ * Represents room database table "steps".
+ * @param id Is id for specific date.
+ * @param date Tells which day steps are.
+ * @param value Tells how many steps user has taken on specific date.
+ * @param previousSteps Tells the value where we started counting steps.
+ * @param userId Refrences users table user id.
+ */
 @Entity(tableName = "steps", indices = [Index(value = ["date"], unique = true)], foreignKeys = [ForeignKey(entity = User::class, parentColumns = arrayOf("id"), childColumns = arrayOf("userId"), onDelete = ForeignKey.CASCADE)])
 data class Steps (
     @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/jhiltunen/stepupcounter/data/models/User.kt
+++ app/src/main/java/com/jhiltunen/stepupcounter/data/models/User.kt
@@ -3,6 +3,14 @@ package com.jhiltunen.stepupcounter.data.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Represents room database table "users".
+ * @param id Is id for specific user.
+ * @param username Users username.
+ * @param height Users height in centimeters.
+ * @param weight Users weight in kilograms.
+ * @param gender Tells users gender.
+ */
 @Entity(tableName = "users")
 data class User (
     @PrimaryKey(autoGenerate = true)
