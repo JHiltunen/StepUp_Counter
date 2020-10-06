d5a5abc77f6fe2ccf409d873a9d6bea8a52295f2
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-10-06 22:01:17 +0300

    Added comments. Changed chart bar colors
    Signed-off-by: Joonas Hiltunen <joonas.s.jh@gmail.com>

--- app/src/main/java/com/jhiltunen/stepupcounter/ui/analytics/AnalyticsFragment.kt
+++ app/src/main/java/com/jhiltunen/stepupcounter/ui/analytics/AnalyticsFragment.kt
@@ -23,8 +23,9 @@ import kotlin.math.pow
 
 class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
 
+    // SimpleDateFormat to help processing Dates
     private val sdfYearMonthDay = SimpleDateFormat("yyyy-MM-dd")
-    private val sdfDayMonth = SimpleDateFormat("dd-MM.")
+    private val sdfDayMonth = SimpleDateFormat("dd.MM.")
 
     @InternalCoroutinesApi
     private lateinit var analyticsViewModel: AnalyticsViewModel
@@ -32,31 +33,48 @@ class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
 
     @InternalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // get the view model
         analyticsViewModel = ViewModelProvider(this).get(AnalyticsViewModel::class.java)
+        // fin bar chart from view
         barChart = view.findViewById(R.id.fragment_verticalbarchart_chart)
 
+        // Get users all steps and observe LiveData from database
+        // when there is change in steps table, draw bar chart again
         analyticsViewModel.getUsersAllSteps.observe(viewLifecycleOwner, {
-
+            // it = list of Steps object
             val labelNames: ArrayList<String> = ArrayList()
-
+            // get dates from List<Steps> to String array
             val dates : Array<Any?> = it.stream().map {steps -> steps.date.toString() }.toArray()
+            // create ArrayList for bar entries/steps values
             val stepsData : ArrayList<BarEntry> = ArrayList()
 
+            // Get steps.value from each Steps object and convert to float. Collect all float values to List<Float>
             val stepsCountArrayList: List<Float> = it.stream().map { steps -> steps.value.toFloat() }.collect(Collectors.toList())
 
+            // loop trough stepsCountArrayList
             for (i in stepsCountArrayList.indices) {
+                // parse yyyy-MM-dd to Date object
+                // then format the date as MM.dd. to show on UI
+                // for example 2020-10-15 will be shown on chart as 10.15.
                 val date: String = sdfDayMonth.format(sdfYearMonthDay.parse(dates[i].toString())).toString()
+                // get the current item from list
                 val stepsCountArrayListItem: Float = stepsCountArrayList[i]
+                // add new bar entry to ArrayList
                 stepsData.add(BarEntry(i.toFloat(), stepsCountArrayListItem))
+                // add date in MM.dd. format to x-axis label names
                 labelNames.add(date)
             }
 
+            // create new data set
             val barDataSet = BarDataSet(stepsData, "Daily steps")
-            barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+            // set color for chart bars
+            barDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
+            // set description for data set
             val description = Description()
-            description.text = "Person"
+            description.text = "Steps/day"
             barChart.description = description
 
+            // format x axis
             val xAxis: XAxis = barChart.xAxis
             xAxis.valueFormatter = IndexAxisValueFormatter(labelNames)
             xAxis.position = XAxis.XAxisPosition.TOP
@@ -66,6 +84,7 @@ class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
             xAxis.labelCount = labelNames.size
             barChart.extraBottomOffset = -50f
 
+            // set data to chart
             val barData = BarData(barDataSet)
             barChart.data = barData
             barData.setValueTextSize(12f)
