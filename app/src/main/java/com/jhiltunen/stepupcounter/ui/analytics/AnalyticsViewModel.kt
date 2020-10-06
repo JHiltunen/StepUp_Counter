5f412ea0c5962b42ebbe80bad23d9539ff540865
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-10-06 22:04:27 +0300

    Added comments
    Signed-off-by: Joonas Hiltunen <joonas.s.jh@gmail.com>

--- app/src/main/java/com/jhiltunen/stepupcounter/ui/analytics/AnalyticsViewModel.kt
+++ app/src/main/java/com/jhiltunen/stepupcounter/ui/analytics/AnalyticsViewModel.kt
@@ -12,12 +12,17 @@ import kotlinx.coroutines.InternalCoroutinesApi
 class AnalyticsViewModel(application: Application) : AndroidViewModel(application) {
 
     private var sharedPreferencesManager : SharedPreferencesManager = SharedPreferencesManager()
+
+    // use LiveData so we can observe if the data in database changes
     val getUsersAllSteps: LiveData<List<Steps>>
     private val repository: HealthRepository
 
     init {
+        // create new Health Data Access Object
         val healthDao = HealthDatabase.getDatabase(application).healthDao()
+        // initialize new repository and load userId from shared preferences
         repository = HealthRepository(healthDao, sharedPreferencesManager.loadUserId(getApplication<Application>().applicationContext))
+        // get list of all steps related to user
         getUsersAllSteps = repository.getAllUsersSteps()
     }
 }
